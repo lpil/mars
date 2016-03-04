2f04bbeff3b5a02eb33f3d953fa3449dec9822ab
Louis Pilfold <louis@lpil.uk>
2016-03-04 00:16:55 +0000

    Exploring planets, one bot at a time.
    Could do with some more attention, but it's way past my bedtime now.

--- lib/mars.ex
+++ lib/mars.ex
@@ -2,4 +2,58 @@ defmodule Mars do
   @moduledoc """
   Exploring planets.
   """
+  alias Mars.Input
+  alias Mars.Instruction
+  alias Mars.Robot
+  alias Mars.World
+
+  @doc """
+  Take some raw instructions and explore the planet.
+  """
+  def explore(input) when is_binary(input) do
+    parsed = Input.parse(input)
+    world  = %World{ max_x: parsed.max_x, max_y: parsed.max_y }
+    explore(world, parsed.instruction_sets)
+  end
+
+
+  defp explore(world, sets, acc \\ [])
+
+  defp explore(_world, [], acc) do
+    acc
+    |> Enum.reverse
+    |> Enum.join("\n")
+  end
+  defp explore(world, [set|sets], acc) do
+    robot = %Robot{} |> Map.merge(set.start)
+    {new_world, result} = explore_set(world, robot, set.instructions)
+    explore(new_world, sets, [result|acc])
+  end
+
+
+  defp explore_set(world, robot, []) do
+    result = robot_to_result(robot)
+    {world, result}
+  end
+
+  defp explore_set(world, robot, [inst|instructions]) do
+    new_robot = Instruction.perform(robot, inst)
+    status = World.check(world, new_robot)
+    cond do
+      status == :lost && World.scent?(world, robot) ->
+        explore_set(world, robot, instructions)
+
+      status == :lost ->
+        scented_world = World.put_scent(world, robot)
+        result = robot_to_result(robot)
+        {scented_world, "#{result} LOST"}
+
+      true ->
+        explore_set(world, new_robot, instructions)
+    end
+  end
+
+  defp robot_to_result(robot) do
+    [robot.x, robot.y, robot.direction] |> Enum.join(" ")
+  end
 end
--- test/mars_test.exs
+++ test/mars_test.exs
@@ -1,8 +1,23 @@
 defmodule MarsTest do
   use ExUnit.Case
-  doctest Mars
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @sample_input """
+  5 3
+  1 1 E
+  RFRFRFRF
+  3 2 N
+  FRRFLLFFRRFLL
+  0 3 W
+  LLFFFLFLFL
+  """
+
+  @sample_output """
+  1 1 E
+  3 3 N LOST
+  2 3 S
+  """ |> String.strip
+
+  test "our little robots can explore mars" do
+    assert Mars.explore(@sample_input) == @sample_output
   end
 end
