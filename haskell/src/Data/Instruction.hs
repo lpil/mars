f78562850453cbf53fafd810f5c6e474055d512c
Louis Pilfold <louis@lpil.uk>
2017-06-27 00:19:26 +0100

    Ohh it gets smaller

--- haskell/src/Data/Instruction.hs
+++ haskell/src/Data/Instruction.hs
@@ -31,19 +31,13 @@ parse :: String -> Maybe [InstructionSet]
 parse = sequence . fmap parseChunk . chunksOf 2 . lines
 
 parseChunk :: [String] -> Maybe InstructionSet
-parseChunk [line1, line2] = do
-  (p, d) <- parseLine1 line1
-  s <- parseLine2 line2
-  Just InstructionSet {startPosition = p, startDirection = d, steps = s}
-parseChunk _ = Nothing
-
-parseLine1 :: String -> Maybe ((Integer, Integer), Bearing)
-parseLine1 [x, ' ', y, ' ', d] = do
+parseChunk [[x, ' ', y, ' ', d], instructions] = do
   x' <- parsePos x
   y' <- parsePos y
   d' <- parseDir d
-  Just ((x', y'), d')
-parseLine1 _ = Nothing
+  s <- sequence $ fmap parseInstruction instructions
+  Just InstructionSet {startPosition = (x', y'), startDirection = d', steps = s}
+parseChunk _ = Nothing
 
 parseDir :: Char -> Maybe Bearing
 parseDir 'N' = Just North
@@ -52,15 +46,13 @@ parseDir 'S' = Just South
 parseDir 'W' = Just West
 parseDir _ = Nothing
 
+parseInstruction :: Char -> Maybe Instruction
+parseInstruction 'F' = Just Advance
+parseInstruction 'L' = Just TurnLeft
+parseInstruction 'R' = Just TurnRight
+parseInstruction _ = Nothing
+
 parsePos :: Char -> Maybe Integer
 parsePos c
   | isDigit c = Just $ read [c]
   | otherwise = Nothing
-
-parseLine2 :: String -> Maybe [Instruction]
-parseLine2 = sequence . fmap toInstruction
-  where
-    toInstruction 'F' = Just Advance
-    toInstruction 'L' = Just TurnLeft
-    toInstruction 'R' = Just TurnRight
-    toInstruction _ = Nothing
